e83a57272f711828c71cee3ee197c56ea664043d
nu11secur1ty <venvaropt@gmail.com>
2020-05-21 21:23:51 +0300

    Update marko.sh

--- marko.sh
+++ marko.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 # 100% nu11secur1ty
+sudo chmod 777 /var/www/html/
+sudo chmod 777 /var/www/html/*
 sudo systemctl start apache2.service
 sudo apt install goaccess -y
 goaccess -f /var/log/apache2/access.log -o /var/www/html/report.html --log-format=COMBINED
