66edb8329e22d030ec7950f581c7d322403c14e7
nu11secur1ty <venvaropt@gmail.com>
2020-05-21 22:22:51 +0300

    Update marko.sh

--- marko.sh
+++ marko.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 # 100% nu11secur1ty
-sudo systemctl stop apache2.service
-sudo systemctl start apache2.service
-sudo apt install goaccess -y
+sudo su - 
+systemctl stop apache2.service
+systemctl start apache2.service
+apt install goaccess -y
 sudo goaccess -f access.log -o /var/www/html/report.html --log-format=COMBINED
   sleep 3
-sudo cp /var/log/apache2/access.log /var/www/html/
+cp /var/log/apache2/access.log /var/www/html/
   cd /var/www/html/
 google-chrome report.html
